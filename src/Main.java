32076cf4a1877a9fb61ce89d3330e3c3c92db6ae
YevgenyVol <yev899@gmail.com>
2020-04-25 09:29:32 +0300

    Final upd

--- src/Main.java
+++ src/Main.java
@@ -17,29 +17,29 @@ public class Main {
 
     @BeforeClass
     public static void setUp() throws IOException, InterruptedException {
+        //capabilities
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("platformName", "Android");
         capabilities.setCapability("deviceName", "Android Device");
         capabilities.setCapability("newCommandTimeout", 120);
         capabilities.setCapability("platformVersion", "6.0.1");
 
+        //package and activity
         capabilities.setCapability("appPackage", "com.android.calculator2");
         capabilities.setCapability("appActivity", "com.android.calculator2.Calculator");
 
         capabilities.setCapability("unicodekeyboard", true);
         capabilities.setCapability("resetkeyboard", true);
-        URL url = new URL("http://0.0.0.0:4723/wd/hub/");
 
+        URL url = new URL("http://0.0.0.0:4723/wd/hub/");
         driver = new AndroidDriver(url, capabilities);
-//        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         Thread.sleep(3000);
     }
 
+    //calculator 3+2 assert method
     @Test
     public void testing(){
-
-
-
         WebElement three = driver.findElement(By.id("com.android.calculator2:id/digit_3")); //button 3
         three.click();
         WebElement plusOp = driver.findElement(By.id("com.android.calculator2:id/op_add")); //button +
@@ -49,10 +49,9 @@ public class Main {
         WebElement equalOp = driver.findElement(By.id("com.android.calculator2:id/eq"));    //button =
         equalOp.click();
 
-        String result = driver.findElement(By.id("com.android.calculator2:id/result")).getText(); //result
-
-
         String expectedResult = "5";
+        String result = driver.findElement(By.id("com.android.calculator2:id/result")).getText(); //actual result
+        //assert results
         try {
             Assert.assertEquals(expectedResult,result);
             System.out.println("result is true , " + result);
@@ -61,10 +60,5 @@ public class Main {
         {
             System.out.println("wrong result , expected  " + expectedResult + " and actual is " + result);
         }
-
-
-
-    }
-
-
-}
+    }//end of testing
+}//end of Main class
